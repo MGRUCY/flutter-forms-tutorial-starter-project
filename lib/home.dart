53591daba7ff53f3b9a87d1f5d3baa09cc96128c
MGRUCY <mgrucy2001@gmail.com>
2025-07-27 20:41:27 +0300

    edges error solved. "NOBODY CARED WHO I WAS TILL I PUT ON THE MASK"

--- lib/home.dart
+++ lib/home.dart
@@ -68,121 +68,113 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
+        title: const Text('Todo List',style: TextStyle(color: Colors.white),),
         centerTitle: true,
-        backgroundColor: Colors.grey[200],
+        backgroundColor: const Color.fromARGB(255, 32, 42, 64),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              SearchBars(
-                onChanged: searchChanged,
-                onSortPressed: sortByPriority,
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            SearchBars(
+              onChanged: searchChanged,
+              onSortPressed: sortByPriority,
+            ),
+            const SizedBox(height: 5),
+            Expanded(
+              child: TodoList(
+                todos: filteredTodos,
+                onDismiss: (index) {
+                  final removedTodo = filteredTodos[index];
+                  setState(() {
+                    todos.remove(removedTodo);
+                    filteredTodos.removeAt(index);
+                  });
+                  saveTodos(todos);
+                },
               ),
-              const SizedBox(height: 5),
-              Expanded(
-                child: TodoList(
-                  todos: filteredTodos,
-                  onDismiss: (index) {
-                    final removedTodo = filteredTodos[index];
-                    setState(() {
-                      todos.remove(removedTodo);
-                      filteredTodos.removeAt(index);
-                    });
-                    saveTodos(todos);
-                  },
-                ),
-              ),
-              const SizedBox(height: 5),
-              SizedBox(
-                height: 200,
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: _formGlobalKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        TextFormField(
-                          maxLength: 20,
-                          decoration: const InputDecoration(
-                            label: Text('Todo title'),
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Type something bud';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) => _title = value!,
-                        ),
-                        TextFormField(
-                          maxLength: 40,
-                          decoration: const InputDecoration(
-                            label: Text('Description'),
-                          ),
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                value.length < 5) {
-                              return 'Type a description at least 5 letters';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) => _description = value!,
-                        ),
-                        const SizedBox(height: 5),
-                        DropdownButtonFormField(
-                          value: _selectedPriority,
-                          decoration: const InputDecoration(
-                            label: Text('Priority'),
-                          ),
-                          items: Priority.values.map((p) {
-                            return DropdownMenuItem(
-                              value: p,
-                              child: Text(p.title),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              _selectedPriority = value!;
-                            });
-                          },
-                        ),
-                        const SizedBox(height: 5),
-                        FilledButton(
-                          onPressed: () {
-                            if (_formGlobalKey.currentState!.validate()) {
-                              _formGlobalKey.currentState!.save();
-                              setState(() {
-                                todos.add(Todo(
-                                  title: _title,
-                                  description: _description,
-                                  priority: _selectedPriority,
-                                ));
-                              });
-                              saveTodos(todos);
-                              _formGlobalKey.currentState!.reset();
-                              _selectedPriority = Priority.low;
-                              loadTodos();
-                            }
-                          },
-                          style: FilledButton.styleFrom(
-                            backgroundColor: Colors.grey[800],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          child: const Text('Add'),
-                        ),
-                      ],
+            ),
+            Form(
+              key: _formGlobalKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextFormField(
+                    maxLength: 20,
+                    decoration: const InputDecoration(
+                      label: Text('Todo title'),
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Type something bud';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => _title = value!,
                   ),
-                ),
+                  TextFormField(
+                    maxLength: 40,
+                    decoration: const InputDecoration(
+                      label: Text('Description'),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.length < 5) {
+                        return 'Type a description at least 5 letters';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => _description = value!,
+                  ),
+                  const SizedBox(height: 5),
+                  DropdownButtonFormField(
+                    value: _selectedPriority,
+                    decoration: const InputDecoration(
+                      label: Text('Priority'),
+                    ),
+                    items: Priority.values.map((p) {
+                      return DropdownMenuItem(
+                        value: p,
+                        child: Text(p.title),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPriority = value!;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 5),
+                  FilledButton(
+                    onPressed: () {
+                      if (_formGlobalKey.currentState!.validate()) {
+                        _formGlobalKey.currentState!.save();
+                        setState(() {
+                          todos.add(Todo(
+                            title: _title,
+                            description: _description,
+                            priority: _selectedPriority,
+                          ));
+                        });
+                        saveTodos(todos);
+                        _formGlobalKey.currentState!.reset();
+                        _selectedPriority = Priority.low;
+                        loadTodos();
+                      }
+                    },
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.grey[800],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    child: const Text('Add'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
